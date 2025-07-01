ad3c7ca85de825f3b6e1573a3a18e19abded11ec
arnavgonella <arnav.gonella@gmail.com>
2025-07-01 16:37:11 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         alt="Riffly Logo"
         width={160}
         height={160}
-        style={{ zIndex: 2 }}
+        style={{ zIndex: 2, borderRadius: '16px' }}
       />
 
       <div style={styles.messageContainer}>
