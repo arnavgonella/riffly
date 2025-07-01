42c30947a5fbc65347ae8e305cc7a9aea04a0f60
arnavgonella <arnav.gonella@gmail.com>
2025-07-01 15:46:52 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,7 +72,7 @@ const styles: { [key: string]: React.CSSProperties } = {
     right: 0,
     bottom: 0,
     backgroundColor: '#5a43c7',
-    opacity: 0.85,
+    opacity: 0.05,
     zIndex: 1,
   },
   message: {
