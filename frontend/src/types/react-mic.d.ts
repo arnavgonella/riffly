ec4c2ed8659fbfe698047a4c9025710d7ebed062
arnavgonella <arnav.gonella@gmail.com>
2025-07-09 22:24:47 -0400

    Update react-mic.d.ts

--- frontend/src/types/react-mic.d.ts
+++ frontend/src/types/react-mic.d.ts
@@ -1,15 +1,14 @@
 declare module 'react-mic' {
-    import * as React from 'react';
-  
-    export interface ReactMicProps {
-      record: boolean;
-      className?: string;
-      onStop: (recordedBlob: any) => void;
-      strokeColor?: string;
-      backgroundColor?: string;
-      mimeType?: string;
-    }
-  
-    export const ReactMic: React.FC<ReactMicProps>;
+  import * as React from 'react';
+
+  export interface ReactMicProps {
+    record: boolean;
+    className?: string;
+    onStop: (recordedBlob: { blob: Blob }) => void;
+    strokeColor?: string;
+    backgroundColor?: string;
+    mimeType?: string;
   }
-  
+
+  export const ReactMic: React.FC<ReactMicProps>;
+}
