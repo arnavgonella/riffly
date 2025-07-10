f29d76463cd159aa3eb30a78f9f4abcd46f5fac0
arnavgonella <arnav.gonella@gmail.com>
2025-07-10 19:01:13 -0400

    Update _app.tsx

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,20 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import { AudioDeviceProvider } from '@/context/AudioDeviceContext';
+// riffly/frontend/src/pages/_app.tsx
 
-export default function App({ Component, pageProps }: AppProps) {
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import '../styles/globals.css'; // adjust or remove if not using global styles
+
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AudioDeviceProvider>
+    <>
+      <Head>
+        <title>Riffly</title>
+        <link rel="icon" href="/favicon.ico" />
+        {/* You can also use PNG: <link rel="icon" type="image/png" href="/favicon.png" /> */}
+      </Head>
       <Component {...pageProps} />
-    </AudioDeviceProvider>
+    </>
   );
 }
 
+export default MyApp;
